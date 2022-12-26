d9032e79065f93b360987d7714729490a0d025ff
thao_ngo <thao_ngo@epam.com>
2022-12-26 15:45:17 +0700

    AB#25-Popular categories menu.

--- src/main/java/com/epam/poc/commons/BasePage.java
+++ src/main/java/com/epam/poc/commons/BasePage.java
@@ -1,12 +1,16 @@
 package com.epam.poc.commons;
 
+import com.epam.poc.utilities.PropertyReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BasePage {
     protected Logger logger;
@@ -35,6 +39,18 @@ public class BasePage {
         return getElement(driver, by).getText();
     }
 
+    public String getElementTextByIndex(List<WebElement> elements, int index) {
+        return elements.get(index).getText();
+    }
+
+    public String getElementValue(WebDriver driver, By by) {
+        return driver.findElement(by).getAttribute("value").toLowerCase();
+    }
+
+    public List<WebElement> getElementWithLimitNumber(List<WebElement> elements, int numberOfElement) {
+        return elements.stream().limit(numberOfElement).collect(Collectors.toList());
+    }
+
     public void sendKeyToElement(WebDriver driver, By by, String value) {
         getElement(driver, by).clear();
         getElement(driver, by).sendKeys(value);
@@ -44,7 +60,18 @@ public class BasePage {
         getElement(driver, by).click();
     }
 
+    public void clickToElementByIndex(List<WebElement> elements, int index) {
+        elements.get(index).isEnabled();
+        elements.get(index).click();
+    }
+
     public boolean isElementDisplayed(WebDriver driver, By by) {
         return getElement(driver, by).isDisplayed();
+    }
+
+    public void waitForElementUntilClickable(WebDriver driver, WebElement element){
+        PropertyReader propertyReader = new PropertyReader(GlobalConstants.CONFIG_FILE_KEY);
+        WebDriverWait wait = new WebDriverWait(driver, Long.parseLong(propertyReader.getValue(GlobalConstants.SHORT_TIMEOUT_KEY)));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 }
--- src/main/java/com/epam/poc/pageObjects/homepage/PopularCategoriesMenuObject.java
+++ src/main/java/com/epam/poc/pageObjects/homepage/PopularCategoriesMenuObject.java
@@ -6,56 +6,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.util.List;
-
 public class PopularCategoriesMenuObject extends BasePage {
     private final WebDriver driver;
 
     public PopularCategoriesMenuObject(WebDriver driver) {
         this.driver = driver;
-    }
-
-    public List<WebElement> getPopularCategoryElements() {
-        return driver.findElements(By.xpath(PopularCategoriesMenuUI.POPULAR_CATEGORIES_MENU_XPATH));
-    }
-
-    public void clickToPopularCategoryByIndex(int index) {
-        List<WebElement> popularCategoryElements = getPopularCategoryElements();
-        popularCategoryElements.get(index).isEnabled();
-        popularCategoryElements.get(index).click();
-    }
-
-    public String getTextPopularCategoryByIndex(int index) {
-        List<WebElement> popularCategoryElements = getPopularCategoryElements();
-        return popularCategoryElements.get(index).getText();
-    }
-
-    public String getShopSearchResultLabel() {
-        return driver.findElement(By.className(PopularCategoriesMenuUI.SHOPEE_SHOP_SEARCH_RESULT_LABEL_CLASS_NAME)).getText();
-    }
-
-    public String getProductSearchResultLabel() {
-        return driver.findElement(By.className(PopularCategoriesMenuUI.SHOPEE_PRODUCT_SEARCH_RESULT_LABEL_CLASS_NAME)).getText();
-    }
-
-    public boolean isShopSearchResultCardItemDisplayed() {
-        WebElement shopSearchResultCardItem = driver.findElement(By.className(PopularCategoriesMenuUI.SHOPEE_SHOP_SEARCH_RESULT_CARD_ITEM_CLASS_NAME));
-        if(shopSearchResultCardItem.isDisplayed()) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public List<WebElement> getProductCards() {
-        return driver.findElements(By.className(PopularCategoriesMenuUI.PRODUCT_CARDS_CLASS_NAME));
-    }
-
-    public String getSearchInputValue() {
-        return driver.findElement(By.xpath(PopularCategoriesMenuUI.SEARCH_INPUT_XPATH)).getAttribute("value").toLowerCase();
-    }
-    public String getProductCardTitle() {
-        return driver.findElement(By.xpath(PopularCategoriesMenuUI.PRODUCT_CARD_TITLE_XPATH)).getText();
     }
 
     public String getProductCardTitleByCard(WebElement element) {

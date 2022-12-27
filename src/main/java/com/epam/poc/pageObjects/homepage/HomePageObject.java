c7ae28688414b38ce9e482d140bd956bf3481504
Tuan_Nguyen4 <Tuan_Nguyen4@epam.com>
2022-12-27 16:57:58 +0700

    AB#25 Update closeHomePagePopup function

--- src/main/java/com/epam/poc/pageObjects/homepage/HomePageObject.java
+++ src/main/java/com/epam/poc/pageObjects/homepage/HomePageObject.java
@@ -1,10 +1,8 @@
 package com.epam.poc.pageObjects.homepage;
 
 import com.epam.poc.commons.BasePage;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePageObject extends BasePage {
 
@@ -20,6 +18,7 @@ public class HomePageObject extends BasePage {
             staticWait(3);
             WebElement closeButton = getElementByJavascriptExecutor(driver,
                     "return document.querySelector('shopee-banner-popup-stateful').shadowRoot.querySelector('.shopee-popup__close-btn')");
+            waitForPageLoadedCompletely(driver);
             waitForElementUntilClickable(driver, closeButton);
             closeButton.click();
             waitForElementUntilInvisible(driver, closeButton);

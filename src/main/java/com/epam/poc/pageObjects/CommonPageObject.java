4566a21cff5b6f2334178c98ff9bcdc396377d26
Duc Le <duclh8@gmail.com>
2023-01-09 16:40:21 +0700

    update ts

--- src/main/java/com/epam/poc/pageObjects/CommonPageObject.java
+++ src/main/java/com/epam/poc/pageObjects/CommonPageObject.java
@@ -1,7 +1,10 @@
 package com.epam.poc.pageObjects;
 
 import com.epam.poc.commons.BasePage;
+import com.epam.poc.pageUIs.CommonPageUI;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
 public class CommonPageObject extends BasePage {
     private final WebDriver driver;
 

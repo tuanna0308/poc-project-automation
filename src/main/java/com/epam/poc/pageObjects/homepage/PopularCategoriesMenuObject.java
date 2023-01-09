3d79bfcd4ce6ddc2dc82197b8fbc359167ddc1da
Nguyen Anh Tuan <31300684+tuanna0308@users.noreply.github.com>
2023-01-09 17:13:30 +0700

    Revert "AB#312 Add Step log for Allure report for tasks in Sprint 3"

--- src/main/java/com/epam/poc/pageObjects/homepage/PopularCategoriesMenuObject.java
+++ src/main/java/com/epam/poc/pageObjects/homepage/PopularCategoriesMenuObject.java
@@ -2,7 +2,6 @@ package com.epam.poc.pageObjects.homepage;
 
 import com.epam.poc.commons.BasePage;
 import com.epam.poc.pageUIs.homepage.PopularCategoriesMenuUI;
-import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,17 +15,14 @@ public class PopularCategoriesMenuObject extends BasePage {
         this.driver = driver;
     }
 
-    @Step("Select category by index '{0}'")
     public void selectCategoryByIndex(int index) {
         clickToElementByIndex(getPopularCategories(), index);
     }
 
-    @Step("Get list popular categories")
     public List<WebElement> getPopularCategories() {
         return getElements(driver, By.xpath(PopularCategoriesMenuUI.POPULAR_CATEGORIES_MENU_XPATH));
     }
 
-    @Step("Check whether '{1}' contains at least 1 word in '{0}' or not")
     public boolean isContainsKeyword(String keyword, String text) {
         boolean isContainsKeyword = false;
         String [] words = keyword.split(" ");
@@ -34,7 +30,6 @@ public class PopularCategoriesMenuObject extends BasePage {
         for (String word: words ) {
             if(text.toLowerCase().contains(word.toLowerCase())) {
                 isContainsKeyword = true;
-                break;
             }
         }
 
